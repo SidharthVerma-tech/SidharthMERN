14bee8396af257e612bdc1828a00a2d941a31248
Sidharth <sidharthvermaiiits@gmail.com>
2024-06-20 02:26:53 +0530

    update frontend

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,17 @@
+import { content as _content, plugin } from "flowbite-react/tailwind";
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('flowbite/plugin'),
-  ],
+export const content = [
+  './index.html',
+  './src/**/*.{js,ts,jsx,tsx}',
+  _content(),
+];
+export const theme = {
+  extend: {},
 };
+export const plugins = [
+  // ...
+  plugin(),
+];
+
+
 
