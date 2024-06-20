c604d11320f6b4dcae82cbdbe88eafb1e8fae8c0
Sidharth <sidharthvermaiiits@gmail.com>
2024-06-20 09:02:14 +0530

    created Footer

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,7 +28,7 @@ export default function Header() {
           <FaMoon />
         </Button>
         <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue'>
+          <Button gradientDuoTone='redToYellow'>
             Sign In
           </Button>
         </Link>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar, TextInput, Button , Label} from 'flowbite-react';
+import { Link,useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { Navbar, TextInput, Button , Label, Alert, Spinner} from 'flowbite-react';
 export default function Signup() {
+  const [formData,setFormData ] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading,setLoading ] = useState(false);
+  const navigate=useNavigate();
+  const handleChange = (e)=>{
+      setFormData({...formData, [e.target.id] : e.target.value.trim()})
+  }
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    if(!formData.username || !formData.email || !formData.password){
+      return setErrorMessage('Please fill out all the details')
+    }
+    try{
+      setLoading(true);
+      setErrorMessage(null);
+      const res= await fetch('api/auth/signup',{
+        method : 'POST',
+        headers : {'Content-Type' : 'application/json'},
+        body: JSON.stringify(formData)
+      })
+      const data = await res.json();
+      if(data.success === false){
+        return setErrorMessage(data.message)
+
+      }
+      setLoading(false)
+      
+      if(res.ok){
+        navigate('/sign-in');
+      }
+    }catch(error){
+      setErrorMessage(error.message)
+      setLoading(false);
+    }
+  }
   return (
+    
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
         {/* left */}
@@ -21,13 +58,14 @@ export default function Signup() {
         {/* right */}
 
         <div className='flex-1'>
-          <form className='flex flex-col gap-4'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
               <Label value='Your username' />
               <TextInput
                 type='text'
                 placeholder='Username'
                 id='username'
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -36,6 +74,7 @@ export default function Signup() {
                 type='email'
                 placeholder='name@company.com'
                 id='email'
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -44,10 +83,20 @@ export default function Signup() {
                 type='password'
                 placeholder='Password'
                 id='password'
+                onChange={handleChange}
               />
             </div>
-            <Button gradientDuoTone='purpleToPink' type='submit'>
-              Sign Up
+            <Button gradientDuoTone='redToYellow' type='submit' disabled={loading}>
+              {
+                loading ? (
+                  <>
+                   <Spinner color='white' size='sm' />
+                   <span>Loading . . .</span>
+                  </>
+                 
+                )
+                : ('Signup')
+              }
             </Button>
             </form>
             <div className='flex gap-2 text-sm mt-5'>
@@ -56,7 +105,13 @@ export default function Signup() {
                 Sign in
               </Link>
             </div>
-
+            {
+              errorMessage && (
+                <Alert className='mt-5' color='failure'>
+                  {errorMessage}
+                </Alert>
+              )
+            }
       </div>
     </div>
     </div>
